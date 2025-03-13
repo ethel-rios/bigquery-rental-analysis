bd897429ae321f808dfa5f6182738c29f26c4451
ethel-rios <yanelirg@gmail.com>
2025-03-12 23:40:14 -0600

    Delete SQL queries/Departments directory

--- SQL queries/Departments/Departments/area_outliers_dep.sql
+++
@@ -1,5 +0,0 @@
-SELECT * 
-FROM rental-market-449821.rents.rentsdata
-WHERE type = "Department" 
-AND area > (SELECT AVG(area) + 2 * STDDEV(area) FROM rental-market-449821.rents.rentsdata)
-ORDER BY area DESC
--- SQL queries/Departments/Departments/price_outliers_dep.sql
+++
@@ -1,5 +0,0 @@
-SELECT * 
-FROM rental-market-449821.rents.rentsdata
-WHERE type = "Department" 
-AND price > (SELECT AVG(price) + 2 * STDDEV(price) FROM rental-market-449821.rents.rentsdata)
-ORDER BY price DESC
