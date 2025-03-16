a70f45e53edd140e2e5d7eaff6b3f08e55f5f923
ethel-rios <yanelirg@gmail.com>
2025-03-16 08:14:58 -0600

    Update least_expensive_neighborhood_dep.sql

--- SQL queries/Departments/least_expensive_neighborhood_dep.sql
+++ SQL queries/Departments/least_expensive_neighborhood_dep.sql
@@ -1,9 +1,15 @@
+WITH ranked AS (
 SELECT
+  DENSE_RANL() OVER (ORDER BY ROUND(AVG(PRICE),2) ASC) AS rank_least,
   municipality,
   neighborhood,
   ROUND(AVG(price),2) AS avg_price
 FROM rental-market-449821.rents.rentsdata
 WHERE type = "Department"
 GROUP BY municipality, neighborhood
-ORDER BY avg_price ASC
-LIMIT 10
+)
+
+SELECT *
+FROM ranked
+WHERE rank_least <= 10
+ORDER BY rank_least ASC;
