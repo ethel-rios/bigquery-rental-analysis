e3acfe03da76eb60a31c2514dd65b17099b52d1f
ethel-rios <yanelirg@gmail.com>
2025-03-16 08:16:08 -0600

    Update most_expesive_neighborhood_house.sql

--- SQL queries/Houses/most_expesive_neighborhood_house.sql
+++ SQL queries/Houses/most_expesive_neighborhood_house.sql
@@ -1,9 +1,15 @@
-SELECT
-  municipality,
-  neighborhood,
-  ROUND(AVG(price),2) AS avg_price
-FROM rental-market-449821.rents.rentsdata
-WHERE type = "House"
-GROUP BY municipality, neighborhood
-ORDER BY avg_price DESC
-LIMIT 10
+WITH ranked AS (
+  SELECT 
+    DENSE_RANK() OVER (ORDER BY ROUND(AVG(price), 2) DESC) AS rank_most,
+    municipality,
+    neighborhood,
+    ROUND(AVG(price), 2) AS avg_price
+  FROM rental-market-449821.rents.rentsdata
+  WHERE type = "House"
+  GROUP BY municipality, neighborhood
+)
+
+SELECT * 
+FROM ranked
+WHERE rank_most <= 10
+ORDER BY rank_most ASC;
