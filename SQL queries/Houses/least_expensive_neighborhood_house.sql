120fea6cd5327b10bd21f9cbbc1c2d6f1403eac3
ethel-rios <yanelirg@gmail.com>
2025-03-16 08:15:49 -0600

    Update least_expensive_neighborhood_house.sql

--- SQL queries/Houses/least_expensive_neighborhood_house.sql
+++ SQL queries/Houses/least_expensive_neighborhood_house.sql
@@ -1,9 +1,15 @@
-SELECT
-  municipality,
-  neighborhood,
-  ROUND(AVG(price),2) AS avg_price
-FROM rental-market-449821.rents.rentsdata
-WHERE type = "House"
-GROUP BY municipality, neighborhood
-ORDER BY avg_price ASC
-LIMIT 10
+WITH ranked AS (
+  SELECT 
+    DENSE_RANK() OVER (ORDER BY ROUND(AVG(price), 2) ASC) AS rank_least,
+    municipality,
+    neighborhood,
+    ROUND(AVG(price), 2) AS avg_price
+  FROM rental-market-449821.rents.rentsdata
+  WHERE type = "House"
+  GROUP BY municipality, neighborhood
+)
+
+SELECT * 
+FROM ranked
+WHERE rank_least <= 10
+ORDER BY rank_least ASC;
